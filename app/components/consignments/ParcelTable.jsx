648f64897363cae2e3ff826f3ae69bd87b153165
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-10-20 14:24:11 +0600

    test

--- app/components/consignments/ParcelTable.jsx
+++ app/components/consignments/ParcelTable.jsx
@@ -44,7 +44,7 @@ const statusMapping = {
     'Payment Processing Complete',
     'Payment Completed',
   ],
-  'Partially Delivered': ['Partially Delivered', 'Assigned To Delivery Rider'],
+  'Partially Delivered': ['Partially Delivered'],
   Cancelled: [
     'Return Confirm',
     'Cancel Order',
